28fdff85f1e35d6510194929a10026232b92ff73
mrchillington <mrchillington@gmail.com>
2019-11-03 01:00:13 -0400

    bop

--- cogs/commands.py
+++ cogs/commands.py
@@ -11,7 +11,10 @@ class Cmd(commands.Cog):
 #mod commands
     @commands.command()
     async def ping(self, ctx):
-        await ctx.send(f"Pong! Server latency = **{round(self.client.latency * 1000)}ms**")
+        embed = discord.Embed(colour=discord.Colour.greyple())
+        embed.add_field(name="**Pong!**", value=f"Server latency: {round(self.client.latency * 1000)}ms")
+        await ctx.send(embed=embed)
+#        await ctx.send(f"Pong! Server latency = **{round(self.client.latency * 1000)}ms**")
 
     @commands.command()
     async def help(self, ctx):
@@ -46,11 +49,48 @@ class Cmd(commands.Cog):
         game = " ".join(arg)
         embed = discord.Embed(
             title = "https://www.twitch.tv/mr_chillington",
-            colour = discord.Colour.dark_purple(),
+            colour = discord.Colour.dark_purple()
         )
         embed.set_author(name="Chillington is currently live on")
         embed.set_thumbnail(url="https://i.imgur.com/NAjJG3E.png")
         embed.add_field(name="Game being played", value=game, inline=True)
+        await ctx.send(embed=embed)
+
+    @commands.command()
+    async def stuff(self, ctx, arg):
+        if arg == "obs":
+#            await ctx.message.delete()
+            embed = discord.Embed()
+            embed.add_field(name="OBS IS BETTER!", value=None)
+            await ctx.send(embed=embed)
+        elif arg == "slobs":
+#            await ctx.message.delete()
+            embed = discord.Embed()
+            embed.add_field(name="SLOBS IS BETTER!", value=None)
+            await ctx.send(embed=embed)
+#    @commands.command()
+#    async def stuff(self, ctx, arg):
+#        if arg == "obs":
+#            await ctx.message.delete()
+#            print("OBS IS BETTER!")
+#        elif arg == "slobs":
+#            await ctx.message.delete()
+#            print("SLOBS IS BETTER!")
+
+    @commands.command()
+    async def obs(self, ctx):
+        embed = discord.Embed(colour=discord.Colour.purple())
+        embed.set_author(name="~OBS Settings~")
+        embed.add_field(name="Encoder:", value=8000, inline=False)
+        embed.add_field(name="Rate Control:", value="CBR", inline=False)
+        embed.add_field(name="Keyframe:", value=2, inline=False)
+        embed.add_field(name="CPU Preset:", value="faster", inline=False)
+        embed.add_field(name="x264 Options:", value="threads=18 rc-lookahead=60 trellis=1 aq-mode=1 direct-pred=spatial", inline=False)
+        embed.add_field(name="Resolution:", value="1280x720", inline=False)
+        embed.add_field(name="Downscale:", value="Bilinear", inline=False)
+        embed.add_field(name="FPS:", value="60", inline=False)
+        embed.add_field(name="Dynamic bitrate:", value=":white_check_mark: ", inline=False)
+        embed.add_field(name="New networking code:", value=":white_check_mark: ", inline=False)
         await ctx.send(embed=embed)
 
 def setup(client):

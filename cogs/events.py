39e104153f6da505ac147e951f2cf0ad9a6b2c85
mrchillington <mrchillington@gmail.com>
2019-11-13 22:35:16 -0500

    bop

--- cogs/events.py
+++ cogs/events.py
@@ -14,27 +14,31 @@ class events(commands.Cog):
             return utils.get(discord.Guild.text_channels, name=channelname)
         def role(self, rolename):
             return utils.get(discord.Guild.roles, name=rolename)
+
     @commands.Cog.listener()
     async def on_error(self, event, *args, **kwargs):
         await utils.get.channel("audit_log").send(f"```{format_exc()[-1994:]}```")
         return
+
     @commands.Cog.listener()
     async def on_command_error(self,ctx,exc):
         if isinstance(exc, CommandNotFound):
             return
         if isinstance(exc, MissingRequiredArgument):
             await ctx.send(":warning: Missing required argument.")
+
     @commands.Cog.listener()
     async def on_reaction_add(self, Reaction, User):
         return
+
     @commands.Cog.listener()
     async def on_reaction_remove(self, Reaction, User):
         return
-    @commands.Cog.listener()
-    async def on_message(self, Message):
-        if not Message.author.bot:
-            await self.client.process_commands(Message)
-        return
+#    @commands.Cog.listener()
+#    async def on_message(self, Message):
+#        if not Message.author.bot:
+#            await self.client.process_commands(Message)
+#        return
 
 def setup(client):
     client.add_cog(events(client))
